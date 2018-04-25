ea0a396a9492f1913c80f11521e766fc6f12ce8a
Kyle <the.big.kb123@gmail.com>
2018-04-24 21:45:12 -0400

    initial commit for test youtube style app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create new component that produces some html
+const App = function() {
+	return <div>hi!</div>;
+}
+// Take this component's generated html and put it on the page (in the dom)
+ReactDOM.render(<App />, document.querySelector('.container'));
