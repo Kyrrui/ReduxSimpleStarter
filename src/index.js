7485156ec379f1a5840ff7a562f2ca4d2220d4ed
Kyle <the.big.kb123@gmail.com>
2018-04-25 23:57:26 -0400

    Continuing to follow tutorial

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyAY6AeWPqXy4jqxyQW4yzwwQeXhhtoHDak';
 
 // Create new component that produces some html
-const App = function() {
-	return <div>hi!</div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ 
+				videos: videos, 
+				selectedVideo: videos[0] 
+			});
+		});
+	}
+	render() {
+		return (
+		<div>
+			<SearchBar />
+			<VideoDetail video={this.state.selectedVideo} />
+			<VideoList 
+			onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+			videos={this.state.videos} />
+		</div>
+		);
+	}
 }
 // Take this component's generated html and put it on the page (in the dom)
 ReactDOM.render(<App />, document.querySelector('.container'));
